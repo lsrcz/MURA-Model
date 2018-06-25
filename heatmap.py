6d0c808f847e2faa52fbed8e8963ec18c7ffe280
aDobeZ <hanghang20080611@126.com>
2018-06-25 23:40:13 +0800

    heatmap demo

---
+++ heatmap.py
@@ -0,0 +1,74 @@
+import io
+import requests
+from model import MURA_Net
+from common import *
+from PIL import Image
+from torchvision import models, transforms
+from torch.autograd import Variable
+from torch.nn import functional as F
+import torch
+import numpy as np
+import cv2
+# networks such as googlenet, resnet, densenet already use global average pooling at the end, so CAM could be used directly.
+net = MURA_Net()
+net = net.to(device)
+net.load_state_dict(torch.load('./models/model.pth'))
+finalconv_name = 'features'
+
+net.eval()
+
+# hook the feature extractor
+features_blobs = []
+def hook_feature(module, input, output):
+    features_blobs.append(output.data.cpu().numpy())
+
+net._modules.get(finalconv_name).register_forward_hook(hook_feature)
+
+# get the softmax weight
+params = list(net.parameters())
+weight_softmax = np.squeeze(params[-2].data.cpu().numpy())
+#print(np.shape(weight_softmax))
+def returnCAM(feature_conv, weight_softmax):
+    # generate the class activation maps upsample to 256x256
+    size_upsample = (256, 256)
+    bz, nc, h, w = feature_conv.shape
+    output_cam = []
+    cam = weight_softmax.dot(feature_conv.reshape((nc, h*w)))
+    cam = cam.reshape(h, w)
+    cam = cam - np.min(cam)
+    cam_img = cam / np.max(cam)
+    cam_img = np.uint8(255 * cam_img)
+    output_cam.append(cv2.resize(cam_img, size_upsample))
+    return output_cam
+
+
+normalize = transforms.Normalize(
+   mean=[0.485, 0.456, 0.406],
+   std=[0.229, 0.224, 0.225]
+)
+preprocess = transforms.Compose([
+   transforms.Resize((224,224)),
+   transforms.ToTensor(),
+   normalize
+])
+
+img_pil = Image.open('./MURA-v1.0/valid/XR_HUMERUS/patient11722/study1_positive/image1.png')
+
+img_tensor = preprocess(img_pil)
+img_variable = Variable(img_tensor.unsqueeze(0)).to(device)
+outputs = net(img_variable)
+preds = (outputs > 0.5).type(torch.LongTensor).numpy()[0]
+
+# generate class activation mapping for the top1 prediction
+CAMs = returnCAM(features_blobs[0], weight_softmax)
+
+
+# render the CAM and output
+print('output CAM.jpg for the top1 prediction: %d'%preds)
+img = cv2.imread('./MURA-v1.0/valid/XR_HUMERUS/patient11722/study1_positive/image1.png')
+height, width, _ = img.shape
+heatmap = cv2.applyColorMap(cv2.resize(CAMs[0],(width, height)), cv2.COLORMAP_JET)
+result = heatmap * 0.3 + img * 0.5
+cv2.imwrite('CAM.jpg', result)
+img = Image.open('CAM.jpg')
+img.show()
