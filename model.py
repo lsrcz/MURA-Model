66c61c66daa07363709f2728e8692348d49f6071
Sirui Lu <lsrcz@pku.edu.cn>
2018-07-02 21:30:17 +0800

    AG net

--- model.py
+++ model.py
@@ -5,6 +5,11 @@ import torch.utils.model_zoo as model_zoo
 import torchvision
 from torchvision import transforms, models
 
+from common import device
+from grad_cam import grad_cam_batch
+from localize import crop_heat
+
+
 class MURA_Net(nn.Module):
     def __init__(self, networkName='densenet169'):
         assert networkName in ['densenet169']
@@ -44,13 +49,36 @@ class MURA_Net_AG(nn.Module):
         self.networkName = networkName
         super(MURA_Net_AG, self).__init__()
         if networkName == 'densenet169':
-            self.global_net = torchvision.models.densenet169(pretrained=True)
-            self.local_net = torchvision.models.densenet169(pretrained=True)
+            self.global_net = MURA_Net(networkName)#torchvision.models.densenet169(pretrained=True)
+            self.local_net = MURA_Net(networkName)#torchvision.models.densenet169(pretrained=True)
             self.classifier = nn.Linear(1664 * 2, 1)
 
-    def forward(self, x):
-        global_features = self.global_net.features(x)
-        # TODO: finish the forward
+    def load_global_dict(self, global_dict):
+        self.global_net.load_state_dict(global_dict)
+
+    def load_local_dict(self, local_dict):
+        self.local_net.load_state_dict(local_dict)
+
+    def forward(self, input):
+        if self.networkName == 'densenet169':
+            global_features = self.global_net.features(input)
+            global_features = F.relu(global_features, inplace=True)
+            global_features = F.avg_pool2d(global_features, kernel_size=7, stride=1)\
+                .view(self.global_net.features.size(0), -1)
+
+            cams = grad_cam_batch(self, input)
+            local_input = crop_heat(cams, input).to(device)
+
+            local_features = self.local_net.features(local_input)
+            local_features = F.relu(local_features, inplace=True)
+            local_features = F.avg_pool2d(local_features, kernel_size=7, stride=1) \
+                .view(self.local_net.features.size(0), -1)
+
+            out = torch.cat([global_features, local_features], dim=1)
+            out = self.classifier(out)
+            out = F.softmax(out)
+            return out
+
 
 
 
