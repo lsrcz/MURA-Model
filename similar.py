cc344b29970836a8938a7326fb95d73e6d4131bd
aDobeZ <hanghang20080611@126.com>
2018-07-03 23:04:06 +0800

    final version

--- similar.py
+++ similar.py
@@ -1,17 +1,15 @@
 from common import *
+from optparse import OptionParser
 from tqdm import tqdm
 import operator
 from dataset import get_dataloaders
-from model import MURA_Net, MURA_Net_Binary
+from model import MURA_Net, get_pretrained_model
 import numpy as np
-from torchvision import models, transforms
+from torchvision import transforms
 from torchvision.datasets.folder import pil_loader
 import cv2
 from PIL import Image
 import matplotlib.pyplot as plt
-from gcam import gcam
-from localize import add_heatmap_ts
-
 from gcam import gcam
 from localize import add_heatmap_ts
 
@@ -77,6 +75,7 @@ def findTOP5addr(model, dataloaders, path):
     path = []
     for i in range(5):
         path.append(sim[i][0])
+        print(sim[i][0])
     return path
 
 def findTOP5pic(model, dataloaders, path):
@@ -88,7 +87,6 @@ def findTOP5pic(model, dataloaders, path):
     img_variable = torch.stack(img_tensors).to(device)
     output, heatmap = gcam(model, img_variable, upsample_size=(320, 320))
     for i in range(0, 5):
-        print(path[i])
         plt.imshow(add_heatmap_ts(heatmap[i], img_variable[i]))
         plt.show()
 
@@ -105,28 +103,31 @@ def main():
     dataloaders, dataset_sizes = get_dataloaders(
         study_name=None,
         data_dir='MURA-v1.0',
-        batch_size=50,
+        batch_size=30,
         batch_eval_ten=15,
         shuffle=False
     )
 
-    # img_path = 'MURA-v1.0/valid/XR_FOREARM/patient11470/study1_positive/image2.png'
+    usage = "usage: %prog [option] img_path"
+    parser = OptionParser(usage)
+    parser.add_option('-i', '--if', help='if only show the address,[y/n]', action='store', type='string', default='n',
+                      dest='if_addr')
 
-    # img_path = 'MURA-v1.0/valid/XR_FOREARM/patient11470/study1_positive/image3.png'
+    options, args = parser.parse_args()
 
-    img_path = 'MURA-v1.0/train/XR_FOREARM/patient09351/study1_positive/image2.png'
+    print('options', options)
+    print('args', args)
 
-    # img_path = 'MURA-v1.0/train/XR_FOREARM/patient09356/study1_positive/image1.png'
+    if len(args) != 1:
+        parser.error('incorrect number of arguments')
 
-    #img_path = 'MURA-v1.0/train/XR_SHOULDER/patient01293/study1_positive/image1.png'
+    img_path = args[0]
+    model = get_pretrained_model('densenet161').to(device)
 
-    model = MURA_Net()
-    model = model.to(device)
-    model.load_state_dict(torch.load('models/model_densenet161_fixed.pth'))
-
-    findTOP5pic(model, dataloaders, img_path)
-
-    #findTOP5addr(model, dataloaders, img_path)
+    if options.if_addr == 'n':
+        findTOP5pic(model, dataloaders, img_path)
+    else:
+        findTOP5addr(model, dataloaders, img_path)
 
 
 if __name__ == '__main__':
